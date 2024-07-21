a492c65aad12a2424d0437288bf2bb5d9a16c3f7
Nguyễn Ngọc Long <51698746+ZNLong2203@users.noreply.github.com>
2024-07-21 12:48:31 +0700

    first commit

--- Contest1/Contest1-H.cpp
+++ Contest1/Contest1-H.cpp
@@ -4,33 +4,36 @@ using namespace std;
 
 const int MAX_A = 1000000;
 
+void solve() {
+	int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> cnt(MAX_A + 1, 0);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        cnt[a[i]]++;
+    }
+
+    int max_gcd = 1;
+    for (int i = MAX_A; i >= 1; i--) {
+        int divisors_cnt = 0;
+        for (int j = i; j <= MAX_A; j += i) {
+            divisors_cnt += cnt[j];
+        }
+        if (divisors_cnt >= 2) {
+            max_gcd = i;
+            break;
+        }
+    }
+
+    cout << max_gcd << endl;
+}
+
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        vector<int> count(MAX_A + 1, 0);
-        
-        for (int i = 0; i < n; ++i) {
-            cin >> a[i];
-            count[a[i]]++;
-        }
-
-        int max_gcd = 1;
-        for (int i = MAX_A; i >= 1; --i) {
-            int divisors_count = 0;
-            for (int j = i; j <= MAX_A; j += i) {
-                divisors_count += count[j];
-            }
-            if (divisors_count >= 2) {
-                max_gcd = i;
-                break;
-            }
-        }
-
-        cout << max_gcd << endl;
+        solve();
     }
-    return 0;
 }
